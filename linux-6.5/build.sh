37daadb7d74eb405bc2d4b40947bfb4abdfa65bc
qiyang.xing <snown@live.cn>
2024-04-05 09:06:34 +0800

    modify Makefile file to distinguish between WSL and Raspberry Pi 4B.

--- linux-6.5/build.sh
+++ linux-6.5/build.sh
@@ -2,14 +2,16 @@
 start_time=$(date +%s)
 export ARCH=arm
 export CROSS_COMPILE=arm-none-linux-gnueabihf-
-export PATH=$PATH:/opt/arm-gnu-toolchain-12.3.rel1-aarch64-arm-none-linux-gnueabihf/bin
+if [ $(arch) = "x86_64" ]; then
+    export PATH=$PATH:/opt/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-linux-gnueabihf/bin
+fi
 echo make distclen
 make distclean
 echo make imx_v6_v7_defconfig
 make imx_v6_v7_defconfig
 #make menuconfig
 echo make
-make -j4   
+make -j8   
 end_time=$(date +%s)
 cost_time=$[ $end_time-$start_time ]
 echo "build kernel time is $(($cost_time/60))min $(($cost_time%60))s"
