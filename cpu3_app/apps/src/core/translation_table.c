1517132d85f6c1a8c239c90a1c5d2122c9096df8
qiyang.xing <snown@live.cn>
2024-04-09 22:13:41 +0800

    set memory type and attributes

--- cpu3_app/apps/src/core/translation_table.c
+++ cpu3_app/apps/src/core/translation_table.c
@@ -4,7 +4,7 @@
 Start Address       End Address  |  SIZE     |   Description                              |   Definition in Translation Table   
 ------------------------------------------------------------------------------------------------------------------------
 0x1000_0000---------0xFFFF_FFFF  |  3840 MB  |   MMDC—DDR Controller.                     | Normal write-back Cacheable 
-0x0800_0000---------0x0FFF_FFFF  |  128 MB   |   EIM—(NOR/SRAM)                           | Normal write-back Cacheable
+0x0800_0000---------0x0FFF_FFFF  |  128 MB   |   EIM—(NOR/SRAM)                           | Normal write-back Cacheable or Device memory (nor flash)
 0x02A0_0000---------0x02DF_FFFF  |  4 MB     |   IPU-2                                    | Device Memory  
 0x0260_0000---------0x029F_FFFF  |  4 MB     |   IPU-1                                    | Device Memory  
 0x0220_C000---------0x023F_FFFF  |  2 MB     |   Reserved                                 | Unassigned
@@ -135,81 +135,82 @@ static void mmu_l1_l2_map(unsigned int pa_start, unsigned int va_start,unsigned 
 void mmu_table_init(void)
 {
     /* 0x0000_0000---------0x0001_7FFF  |  96 KB    |   Boot ROM (ROMCP)                         |*/
-    mmu_l1_l2_map(0x00000000, 0x00000000,0x1800,0,0);
+    mmu_l1_l2_map(0x00000000, 0x00000000,0x1800,0x001,0x636);// device APX=1 AP=11 read only 
     /* 0x0001_8000---------0x000F_FFFF  |  928 KB   |   Reserved                                 |*/
     mmu_l1_l2_map(0x00018000, 0x00018000,0xe8000,0x0,0x0);
     /* 0x0010_0000---------0x0010_3FFF  |  16 KB    |   CAAM (16 KB secure RAM)                  |*/
-    mmu_l1_l2_map(0x00100000, 0x00100000,0x4000,0x0,0x0);
+    mmu_l1_l2_map(0x00100000, 0x00100000,0x4000,0x001,0x436);
     /* 0x0010_4000---------0x0010_FFFF  |  48 KB    |   Reserved                                 |*/
     mmu_l1_l2_map(0x00104000, 0x00104000,0xc000,0x0,0x0);
     /* 0x0011_0000---------0x0011_1FFF  |  8 KB     |   APBH-Bridge-DMA                          |*/
-    mmu_l1_l2_map(0x00110000, 0x00110000,0x2000,0x0,0x0);
+    // l1 P=1'b0,Domain = 4'b0 ,SBZ = 3'b0,ID = 2'b01; l2 nG = 1'b0,S = 1'b1,APX = 1'b0,TEX = 3'b000 AP = 2'b11, C = 1'b0,B=1'b1,XN=1'b0
+    mmu_l1_l2_map(0x00110000, 0x00110000,0x2000,0x001,0x436);
     /* 0x0011_2000---------0x0011_3FFF  |  8 KB     |   GPMI                                     |*/
-    mmu_l1_l2_map(0x00112000, 0x00112000,0x2000,0x0,0x0);
+    mmu_l1_l2_map(0x00112000, 0x00112000,0x2000,0x001,0x436);
     /* 0x0011_4000---------0x0011_7FFF  |  16 KB    |   BCH                                      |*/
-    mmu_l1_l2_map(0x00114000, 0x00114000,0x4000,0x0,0x0);
+    mmu_l1_l2_map(0x00114000, 0x00114000,0x4000,0x001,0x436);
     /* 0x0011_8000---------0x0011_FFFF  |  32 KB    |   Reserved                                 |*/
     mmu_l1_l2_map(0x00118000, 0x00118000,0x8000,0x0,0x0);
     /* 0x0012_0000---------0x0012_8FFF  |  36 KB    |   HDMI                                     |*/
-    mmu_l1_l2_map(0x00120000, 0x00120000,0x9000,0x0,0x0);
+    mmu_l1_l2_map(0x00120000, 0x00120000,0x9000,0x001,0x436);
     /* 0x0012_9000---------0x0012_FFFF  |  28 KB    |   Reserved                                 |*/
     mmu_l1_l2_map(0x00129000, 0x00129000,0x7000,0x0,0x0);
     /* 0x0013_0000---------0x0013_3FFF  |  16 KB    |   GPU 3D                                   |*/
-    mmu_l1_l2_map(0x00130000, 0x00130000,0x4000,0x0,0x0);
+    mmu_l1_l2_map(0x00130000, 0x00130000,0x4000,0x001,0x436);
     /* 0x0013_4000---------0x0013_7FFF  |  16 KB    |   GPU 2D (GC320)                           |*/
-    mmu_l1_l2_map(0x00134000, 0x00134000,0x4000,0x0,0x0);
+    mmu_l1_l2_map(0x00134000, 0x00134000,0x4000,0x001,0x436);
     /* 0x0013_8000---------0x0013_BFFF  |  16 KB    |   DTCP                                     |*/
-    mmu_l1_l2_map(0x00138000, 0x00138000,0x4000,0x0,0x0);
+    mmu_l1_l2_map(0x00138000, 0x00138000,0x4000,0x001,0x436);
     /* 0x0013_C000---------0x001F_FFFF  |  784 KB   |   Reserved                                 |*/
     mmu_l1_l2_map(0x0013c000, 0x0013c000,0xc4000,0x0,0x0);
     /* 0x0020_0000---------0x002F_FFFF  |  1 MB     |   GPV_2 PL301 (per1) configuration port    |*/
-    mmu_l1_l2_map(0x00200000, 0x00200000,0x100000,0x0,0x0);
+    mmu_l1_l2_map(0x00200000, 0x00200000,0x100000,0x001,0x436);
     /* 0x0030_0000---------0x003F_FFFF  |  1 MB     |   GPV_3 PL301 (per2) configuration port    |*/
-    mmu_l1_l2_map(0x00300000, 0x00300000,0x100000,0x0,0x0);
+    mmu_l1_l2_map(0x00300000, 0x00300000,0x100000,0x001,0x436);
     /* 0x0040_0000---------0x007F_FFFF  |  4 MB     |   Reserved                                 |*/
     mmu_l1_l2_map(0x00400000, 0x00400000,0x400000,0x0,0x0);
     /* 0x0080_0000---------0x008F_FFFF  |  1 MB     |   GPV_4 PL301 (fast3) configuration port   |*/
-    mmu_l1_l2_map(0x00800000, 0x00800000,0x100000,0x0,0x0);
+    mmu_l1_l2_map(0x00800000, 0x00800000,0x100000,0x001,0x436);
     /* 0x0090_0000---------0x0093_FFFF  |  0.25 MB  |   OCRAM 256 KB                             |*/
-    mmu_l1_l2_map(0x00900000, 0x00900000,0x40000,0x0,0x0);
+    mmu_l1_l2_map(0x00900000, 0x00900000,0x40000,0x1e1,0x13e);// inner-cacheable
     /* 0x0094_0000---------0x009F_FFFF  |  0.75 MB  |   OCRAM aliased                            |*/
-    mmu_l1_l2_map(0x00940000, 0x00940000,0xc0000,0x0,0x0);
+    mmu_l1_l2_map(0x00940000, 0x00940000,0xc0000,0x1e1,0x13e);// inner-cacheable
     /* 0x00A0_0000---------0x00A0_1FFF  |  8 KB     |   ARM MP                                   |*/
-    mmu_l1_l2_map(0x00a00000, 0x00a00000,0x2000,0x0,0x0);
+    mmu_l1_l2_map(0x00a00000, 0x00a00000,0x2000,0x001,0x436);
     /* 0x00A0_2000---------0x00A0_2FFF  |  4 KB     |   PL310 (L2 cache controller)              |*/
-    mmu_l1_l2_map(0x00a02000, 0x00a02000,0x1000,0x0,0x0);
+    mmu_l1_l2_map(0x00a02000, 0x00a02000,0x1000,0x001,0x436);
     /* 0x00A0_3000---------0x00AF_FFFF  |  1012 KB  |   Reserved                                 |*/
     mmu_l1_l2_map(0x00a03000, 0x00a03000,0xfd000,0x0,0x0);
     /* 0x00B0_0000---------0x00BF_FFFF  |  1 MB     |   GPV_0 PL301 (fast2) configuration port   |*/
-    mmu_l1_l2_map(0x00b00000, 0x00b00000,0x100000,0x0,0x0);
+    mmu_l1_l2_map(0x00b00000, 0x00b00000,0x100000,0x001,0x436);
     /* 0x00C0_0000---------0x00CF_FFFF  |  1 MB     |   GPV_1 PL301 (fast1)                      |*/
-    mmu_l1_l2_map(0x00c00000, 0x00c00000,0x100000,0x0,0x0);
+    mmu_l1_l2_map(0x00c00000, 0x00c00000,0x100000,0x001,0x436);
     /* 0x00D0_0000---------0x00FF_FFFF  |  3072 KB  |   Reserved                                 |*/
     mmu_l1_l2_map(0x00d00000, 0x00d00000,0x300000,0x0,0x0);
     /* 0x0100_0000---------0x01FF_BFFF  |  16368 KB |   PCIe                                     |*/
-    mmu_l1_l2_map(0x01000000, 0x01000000,0xffc000,0x0,0x0);
+    mmu_l1_l2_map(0x01000000, 0x01000000,0xffc000,0x001,0x436);
     /* 0x01FF_C000---------0x01FF_FFFF  |  16 KB    |   PCIe registers                           |*/
-    mmu_l1_l2_map(0x01ffc000, 0x01ffc000,0x4000,0x0,0x0);
+    mmu_l1_l2_map(0x01ffc000, 0x01ffc000,0x4000,0x001,0x436);
     /* 0x0200_0000---------0x020F_FFFF  |  1 MB     |   Peripheral IPs via AIPS-1                |*/
-    mmu_l1_l2_map(0x02000000, 0x02000000,0x100000,0x0,0x0);
+    mmu_l1_l2_map(0x02000000, 0x02000000,0x100000,0x001,0x436);
     /* 0x0210_0000---------0x021F_FFFF  |  1 MB     |   Peripheral IPs via AIPS-2                |*/
-    mmu_l1_l2_map(0x02100000, 0x02100000,0x100000,0x0,0x0);
+    mmu_l1_l2_map(0x02100000, 0x02100000,0x100000,0x001,0x436);
     /* 0x0220_0000---------0x0220_3FFF  |  16 KB    |   SATA                                     |*/
-    mmu_l1_l2_map(0x02200000, 0x02200000,0x4000,0x0,0x0);
+    mmu_l1_l2_map(0x02200000, 0x02200000,0x4000,0x001,0x436);
     /* 0x0220_4000---------0x0220_7FFF  |  16 KB    |   OpenVG (GC355)                           |*/
-    mmu_l1_l2_map(0x02204000, 0x02204000,0x4000,0x0,0x0);
+    mmu_l1_l2_map(0x02204000, 0x02204000,0x4000,0x001,0x436);
     /* 0x0220_8000---------0x0220_BFFF  |  16 KB    |   MIPI_HSI                                 |*/
-    mmu_l1_l2_map(0x02208000, 0x02208000,0x4000,0x0,0x0);
+    mmu_l1_l2_map(0x02208000, 0x02208000,0x4000,0x001,0x436);
     /* 0x0220_C000---------0x023F_FFFF  |  2 MB     |   Reserved                                 |*/
     mmu_l1_l2_map(0x0220c000, 0x0220c000,0x200000,0x0,0x0);
     /* 0x0260_0000---------0x029F_FFFF  |  4 MB     |   IPU-1                                    |*/
-    mmu_l1_l2_map(0x02600000, 0x02600000,0x400000,0x0,0x0);
+    mmu_l1_l2_map(0x02600000, 0x02600000,0x400000,0x001,0x436);
     /* 0x02A0_0000---------0x02DF_FFFF  |  4 MB     |   IPU-2                                    |*/
-    mmu_l1_l2_map(0x02a00000, 0x02a00000,0x400000,0x0,0x0);
+    mmu_l1_l2_map(0x02a00000, 0x02a00000,0x400000,0x001,0x436);
     /* 0x0800_0000---------0x0FFF_FFFF  |  128 MB   |   EIM—(NOR/SRAM)                           |*/
-    mmu_l1_l2_map(0x08000000, 0x0800000,0x8000000,0x0,0x0);
+    mmu_l1_l2_map(0x08000000, 0x0800000,0x8000000,0x001,0x436);
     /* 0x1000_0000---------0xFFFF_FFFF  |  3840 MB  |   MMDC—DDR Controller.                     |*/
-    mmu_l1_l2_map(0x10000000, 0x10000000,0xf0000000,0x0,0x0);
+    mmu_l1_l2_map(0x10000000, 0x10000000,0xf0000000,0x1e1,0x576);// Shareable, Outer and Inner Write-Back, Write-Allocate cache 
     disp("translation table init done \n");   
     return;
 }
